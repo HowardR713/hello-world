c0734e679d091b2b3d6b477cd6dd86f111e71a74
Howard Riley <c.riley01@verizon.net>
2020-09-26 13:37:35 -0400

    Successful use of decimal library

--- say-hi.py
+++ say-hi.py
@@ -1,2 +1,35 @@
-print ("hello to the world!")
-print ( 2 * 2 )
+# Get the decimal libs:
+from decimal import *
+
+def mathify( sInput ):
+    d1 = Decimal( sInput )
+    mSign = d1.as_tuple().sign
+    mExp = d1.as_tuple().exponent
+    mInt = abs( int( d1 * ( 10 ** abs( mExp ) ) ) )
+    mathTuple = ( mSign, mInt, mExp )
+    return mathTuple
+
+# Get input numbers:
+s1 = "-6543.21"
+t1 = "42"
+
+# Process s1:
+
+# Print out everything:
+print("=======================")
+print("Hello world!")
+mTuple = mathify( s1 )
+print( s1 )
+print( "sign", mTuple[0] )
+print( "integer", mTuple[1] )
+print( "exponent", mTuple[2] )
+
+print("-----------------------")
+mTuple = mathify( t1 )
+print(t1)
+print( "sign", mTuple[0] )
+print( "integer", mTuple[1] )
+print( "exponent", mTuple[2] )
+print ("goodbye world!")
+print("=======================")
+
