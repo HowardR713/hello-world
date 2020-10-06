33e526e30f5af17da12d4c97f26cedbc40f0f1f2
Howard Riley <c.riley01@verizon.net>
2020-10-05 21:11:37 -0400

    Further streamlining and bug fixes.

--- say-hi.py
+++ say-hi.py
@@ -10,31 +10,31 @@ def execOps(op,t1,t2):
     d2 = math_lib.mymath( t2 )
     # Make decisions based on the operation:
     if (op=='+'):
-        # Addition -> sum:
-        msg = "Addition:"
-        ansMsg = "Sum:"
-        d3 = d1.add( d2 )
+        # Addition -> Sum:
+        msg     = "Addition:"
+        ansMsg  = "Sum:"
+        d3      = d1.add( d2 )
     elif (op=='-'):
-        # Subtraction -> difference:
-        msg = "Subtraction"
-        ansMsg = "Difference:"
-        d3 = d1.subtract( d2 )
+        # Subtraction -> Difference:
+        msg     = "Subtraction:"
+        ansMsg  = "Difference:"
+        d3      = d1.subtract( d2 )
     elif (op=="*"):
-        # Multiplication -> product:
-        msg = "Multiplication"
-        ansMsg = "Product:"
-        d3 = d1.multiply( d2 )
+        # Multiplication -> Product:
+        msg     = "Multiplication:"
+        ansMsg  = "Product:"
+        d3      = d1.multiply( d2 )
     elif (op=="/"):
-        # Division -> quotient:
-        msg = "Division"
-        ansMsg = "Quotient:"
+        # Division -> Quotient:
+        msg     = "Division:"
+        ansMsg  = "Quotient:"
         if (d2.mInt==0):
-            msg = "ERROR:  Can't divide by 0."
+            msg     = "ERROR:  Can't divide by 0."
             isError = 1
         else:
-            d3 = d1.divide( d2 )
+            d3      = d1.divide( d2 )
     else:
-        msg = "ERROR:  Invalid operation: "
+        msg     = "ERROR:  Invalid operation: " + op
         isError = 1
     if (isError==0):
         print( msg, "-----------------------")
@@ -45,12 +45,8 @@ def execOps(op,t1,t2):
         print( ansMsg, "-----------------------")
         d3.dump()
     else:
-        print(ansMsg,op)
+        print(msg)
     print("-----------------------")
-
-# Get input numbers:
-t1 = "-6543.21"
-t2 = "42.00001"
 
 # Let's try some math.  Print out everything:
 print("=======================")
@@ -72,5 +68,14 @@ execOps("*",t1,t2)
 t1 = "8.104012345"
 t2 = "5.0002"
 execOps("/",t1,t2)
+# -----------------------
+t1 = "8.104012345"
+t2 = "0"
+execOps("/",t1,t2)
+# -----------------------
+t1 = "8.104012345"
+t2 = "0"
+execOps("bs",t1,t2)
+
 print ("goodbye world!")
 print("=======================")
